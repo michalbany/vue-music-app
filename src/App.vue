405518da533b72c33cb6c6283fb6fab35afd5170
michalbany <mobany526@gmail.com>
2024-02-26 16:32:18 +0100

    autentifikace uzivatele pres firebase

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup>
 import AppHeader from '@/components/AppHeader.vue'
 import AppAuth from '@/components/AppAuth.vue'
+import { useUserStore } from './stores/user'
+import { auth } from './includes/firebase'
+import { watchEffect } from 'vue'
+
+const userStore = useUserStore()
+
+watchEffect(() => {
+  if (auth.currentUser) {
+    userStore.userLoggedIn = true
+  }
+})
 </script>
 
 <template>
