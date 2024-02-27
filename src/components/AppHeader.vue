56dab3d2aa5355a1e1b7dbc49f03e04ffcdaa5ab
michalbany <mobany526@gmail.com>
2024-02-27 11:43:51 +0100

    added new router

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,12 +17,15 @@ function toggleAuthModal() {
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <RouterLink class="text-white font-bold uppercase text-2xl mr-4" :to="{ name: 'home' }" >Music</RouterLink>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <RouterLink class="px-2 text-white" :to="{ name: 'about' }" >About</RouterLink>
+          </li>
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
               >Login / Register</a
@@ -30,7 +33,7 @@ function toggleAuthModal() {
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <RouterLink class="px-2 text-white" :to="{ name: 'manage' }">Manage</RouterLink>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="userStore.signout">logout</a>
