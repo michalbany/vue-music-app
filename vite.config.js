e3b8ae96f98096e3bccda8fec4bf9010f8d921cd
Michael Bany <mobany526@gmail.com>
2024-03-07 17:18:00 +0100

    added visualizer

--- vite.config.js
+++ vite.config.js
@@ -3,29 +3,31 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
+// import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // visualizer({ open: true }),
     VitePWA({
       injectRegister: 'auto',
-      registerType: "autoUpdate",
+      registerType: 'autoUpdate',
       devOptions: {
-        enabled: true,
+        enabled: true
       },
       manifest: {
-        name: "Music App",
-        theme_color: "#ff5e3a",
+        name: 'Music App',
+        theme_color: '#ff5e3a',
         icons: [
           {
-            src: "assets/img/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
+            src: 'assets/img/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
           }
         ]
-      },
-    }),
+      }
+    })
   ],
   resolve: {
     alias: {
